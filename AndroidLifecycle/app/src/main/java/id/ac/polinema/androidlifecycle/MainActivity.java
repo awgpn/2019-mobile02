ef76066180bf96c8ddca92dc2f47a0201383dccf
awgpn <dhanawinks@gmail.com>
2024-03-27 13:19:45 +0800

    coba

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -13,9 +13,36 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	// TODO: tambahkan callback onStart() di sini
-
+protected void onStart() {
+		super.onStart();
+	android.widget.Toast.makeText(context:, "App on start", Toast.LENGTH_SHORT).show();
+}
 
 	// TODO: tambahkan callback onStop() di sini
-
+protected void onStop(){
+		super.onStop();
+	android.widget.Toast.makeText(context:, "App on stop", Toast.LENGTH_SHORT).show();
+}
 	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
+	protected void onRestart(){
+		super.onRestart();
+		android.widget.Toast.makeText(context:, "App on Restart", Toast.LENGTH_SHORT).show();
+	}
+	// on reasume
+	protected void onResume(){
+		super.onResume();
+		android.widget.Toast.makeText(context:, "App on Resume", Toast.LENGTH_SHORT).show();
+	}
+	//on pause
+	protected void onPause(){
+
+		super.onPause();
+		android.widget.Toast.makeText(context, "App on Pause", Toast.LENGTH_SHORT).show();
+	}
+
+	//on destroy
+	protected void onDestroy(){
+		super.onDestroy();
+		android.widget.Toast.makeText(context:, "App on destroy", Toast.LENGTH_SHORT).show();
+	}
 }
